2c7033bc84beeaaea03049863c3c3caf2237213e
mombouli1998 <trinitemombouli@gmail.com>
2025-07-02 22:31:18 +0200

    deployements de l'application sur streamlit cloud

--- streamlit_app/page0.py
+++ streamlit_app/page0.py
@@ -13,7 +13,7 @@ col1, col2, col3 = st.columns([1, 4, 1])
 # Colonne gauche : Image
 with col1:
     st.image(
-        "#",  # Remplacez par le chemin de votre image
+        "https://i.goopics.net/95gv5t.jpg",  # Remplacez par le chemin de votre image
         width=80,     # Ajustez la taille si nécessaire
         use_container_width=False,
     )
@@ -46,7 +46,7 @@ st.write(" ")
 # Titre
 st.markdown("# **Phase 1 : Développeur Python & Architecte API**")
 # Afficher l'image séparément
-st.image("https://i.goopics.net/71jq5d.pngls", use_container_width=True)
+# st.image("https://i.goopics.net/71jq5d.png", use_container_width=True)
 
 st.markdown(
         """
